45863eeb1bbff5565e390967e3966d3ed56c5663
kalipolis <1753573997@qq.com>
2025-04-09 10:25:19 +0800

    三维重建子菜单

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QColorDialog>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->colorButton, &QPushButton::clicked, this, &MainWindow::onColorButtonClicked);
 }
 
 MainWindow::~MainWindow()
@@ -13,7 +14,17 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
+void MainWindow::onColorButtonClicked()
+{
+    QColorDialog *cdia = new QColorDialog(this);
+    cdia->setWindowTitle("选择颜色");
+    cdia->setCurrentColor(QColor("red"));
+    if (cdia->exec() == QDialog::Accepted) {
+        QColor color = cdia->currentColor();
+        ui->colorButton->setStyleSheet(QString("background-color: %1").arg(color.name()));
+    }
+    delete cdia;
+}
 
 
 void MainWindow::on_radioButton_surgery_toggled(bool checked)
