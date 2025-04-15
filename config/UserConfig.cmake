10f3df6d7854e48863a862482e9cf3321a80e8ab
jinguo <1364332376@qq.com>
2025-04-15 15:49:12 +0800

    修改vtk库路径配置文件

---
+++ config/UserConfig.cmake
@@ -0,0 +1,5 @@
+if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(VTK_DIR "D:/Toolkit/VTK/VTK-9.2.0.rc2-install-debug/lib/cmake/vtk-9.2")
+else()
+    set(VTK_DIR "D:/Toolkit/VTK/VTK-9.2.0.rc2-install-release/lib/cmake/vtk-9.2")
+endif()
